8a23e89d9d9ba34729cf3917abd8f6b0c63b1752
Taliss <ivvlahov@yahoo.com>
2016-02-10 15:09:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+let path = require('path');
+let fs = require('fs-extra-promise');
+let Promise = require('bluebird');
+
+module.exports = function (mikser, context) {
+		let tocInfo = {
+			collection: context.data[context.layout.meta.toc] || context.data['toc'],
+			destinationFolder: path.join(mikser.options.workingFolder, mikser.config.tocFolder || 'toc'),
+			ext: '.txt',
+			getDestination: function() {
+				let ext = path.extname(context.document.destination)
+				return context.document.destination.replace(mikser.config.outputFolder, this.destinationFolder).replace(ext, this.ext);
+			},
+			content: []
+		}
+
+		if(!Array.isArray(tocInfo.collection)) {
+			let err = new Error('Collection is required');
+			err.origin = 'toc';
+			throw err;
+		}
+
+	context.process(() => {
+		return Promise.each(tocInfo.collection, (document) => {
+			return fs.readFileAsync(document.source).then((content) => {
+				tocInfo.content.push(new Buffer('[' + document._id + ']' + '\n' + content + '\n\n'));
+			}).then(() => {
+				return fs.outputFileAsync(tocInfo.getDestination(), Buffer.concat(tocInfo.content));
+			});
+		});
+	});
+}
